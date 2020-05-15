6e8753db83aabe815dcfc45bfab6a7844ddcb6b5
mona <hibaalaani@github.com>
2020-05-15 11:35:24 +0200

    check profile

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -80,6 +81,9 @@ export const login = (email, password) => async (dispatch) => {
 };
 //LOGOUT/Clear profile
 export const logout = () => (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
   dispatch({
     type: LOGOUT,
   });
