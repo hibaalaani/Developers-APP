9c4e61752d50e515f48d555690ab19bc85e1d8ae
mona <hibaalaani@github.com>
2020-05-29 12:59:00 +0200

    new githubToken

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -9,11 +9,11 @@ const Experience = ({ experience, deleteExperience }) => {
       <td>{exp.company}</td>
       <td className="hide-sm">{exp.title}</td>
       <td>
-        <Moment format="YYYY/MM/DD">{Experience.form}</Moment> -{" "}
+        <Moment format="YYYY/MM/DD">{exp.from}</Moment> -{" "}
         {exp.to === null ? (
           "Now"
         ) : (
-          <Moment format="YYYY/MM/DD">{Experience.TO}</Moment>
+          <Moment format="YYYY/MM/DD">{exp.to}</Moment>
         )}
       </td>
       <td>
